1ff471d54821d79cc6756b4b49733574714fdfd5
SnooMac <broooo0336@gmail.com>
2024-04-22 20:35:56 +0300

    ver. 0.003

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from pygame.locals import QUIT
 # Adjust dimensions for the rooms to fit within the screen
 ROOM_WIDTH = 300
 ROOM_HEIGHT = 200
-SCREEN_WIDTH = 900
+SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 600
 
-rooms = [[random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
-         [random.randint(0, 1), 1, random.randint(0, 1)],
-         [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)]
+rooms = [[random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
+         [random.randint(0, 1), 1, 1, 1],
+         [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)]
          ]
 
 
@@ -30,14 +30,34 @@ class Create_rooms:
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+rect = pygame.Rect(40, 40, 12, 12)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                rect.move_ip(-40, 0)
+            elif event.key == pygame.K_RIGHT:
+                rect.move_ip(40, 0)
+            elif event.key == pygame.K_UP:
+                rect.move_ip(0, -40)
+            elif event.key == pygame.K_DOWN:
+                rect.move_ip(0, 40)
+            elif event.key == pygame.K_a:
+                rect.move_ip(-40, 0)
+            elif event.key == pygame.K_d:
+                rect.move_ip(40, 0)
+            elif event.key == pygame.K_w:
+                rect.move_ip(0, -40)
+            elif event.key == pygame.K_s:
+                rect.move_ip(0, 40)
+
     screen.fill((0, 0, 0))  # Fill the screen with black before drawing anything
     Create_rooms(rooms, screen).create()
+    pygame.draw.rect(screen, (255, 0, 0), rect, 0)
     pygame.display.flip()  # Update the display to show changes
     clock.tick(24)
 
