6d92a14fba517b7f508531aeca2c44a8a1a1f2df
SnooMac <broooo0336@gmail.com>
2024-05-08 16:53:51 +0300

    ver. 0.0046

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ class Create_rooms(Wall):
         super().__init__(screen)
         self.rect_list = []
         self.rooms = rooms
+        self.doors = []
 
     def create_rooms(self):
         y_top = 0
@@ -68,29 +69,41 @@ class Create_rooms(Wall):
                     self.rect_list.append(right_wall)
                     self.rect_list.append(top_wall)
 
-                    if x < len(self.rooms[y]) - 1 and self.rooms[y][x + 1]:
-                        pygame.draw.rect(self.screen, (0, 255, 0),
-                                         (x * WALL_WIDTH + WALL_WIDTH - 15, y * WALL_HEIGHT + WALL_HEIGHT // 2 - 5, 45,
-                                          50), 0)
-                    if y < len(self.rooms) - 1 and self.rooms[y + 1][x]:
-                        pygame.draw.rect(self.screen, (0, 255, 0),
-                                         (x * WALL_WIDTH + WALL_WIDTH // 2 - 5, y * WALL_HEIGHT + WALL_HEIGHT - 15, 45,
-                                          50), 0)
-
-            # Update positions for next row of rooms
             y_top += 200
             y_bottom += 200
             x_left += 300
 
+    def create_doors(self):
+        for y, level in enumerate(self.rooms):
+            for x, room in enumerate(level):
+                if room:
+                    if x < len(self.rooms[y]) - 1 and self.rooms[y][x + 1]:
+                        pygame.draw.rect(self.screen, (0, 255, 0),
+                                         (x * WALL_WIDTH + WALL_WIDTH - 15, y * WALL_HEIGHT + WALL_HEIGHT // 2 - 5, 45,
+                                          50), 0)
+                        self.doors.append(
+                            pygame.Rect(x * WALL_WIDTH + WALL_WIDTH - 15, y * WALL_HEIGHT + WALL_HEIGHT // 2 - 5, 45,
+                                        50))
+                    if y < len(self.rooms) - 1 and self.rooms[y + 1][x]:
+                        pygame.draw.rect(self.screen, (0, 255, 0),
+                                         (x * WALL_WIDTH + WALL_WIDTH // 2 - 5, y * WALL_HEIGHT + WALL_HEIGHT - 15, 45,
+                                          50), 0)
+                        self.doors.append(
+                            pygame.Rect(x * WALL_WIDTH + WALL_WIDTH // 2 - 5, y * WALL_HEIGHT + WALL_HEIGHT - 15, 45,
+                                        50))
+
     def get_rect_list(self):
         return self.rect_list
+
+    def get_door_list(self):
+        return self.doors
 
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 telega = pygame.image.load('priest1_v1_1.png').convert_alpha()
 clock = pygame.time.Clock()
-player = Player(580, 290, 10, 10, screen)
+player = Player(560, 290, 10, 10, screen)
 rect = player.get_player()
 
 done = False
@@ -101,25 +114,49 @@ while not done:
 
     level = Create_rooms(rooms, screen)
     level.create_rooms()
+    level.create_doors()
     rect_list = level.get_rect_list()
+    door_list = level.get_door_list()
 
     screen.fill((0, 0, 0))
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
-        rect.x -= speed if not rect.collidelistall(rect_list) else -10
+        if rect.collidelistall(door_list):
+            rect.x -= 35
+        elif not rect.collidelistall(rect_list):
+            rect.x -= speed
+        else:
+            rect.x += 20
     elif keys[pygame.K_d]:
-        rect.x += speed if not rect.collidelistall(rect_list) else -10
+        if rect.collidelistall(door_list):
+            rect.x += 35
+        elif not rect.collidelistall(rect_list):
+            rect.x += speed
+        else:
+            rect.x -= 20
     elif keys[pygame.K_s]:
-        rect.y += speed if not rect.collidelistall(rect_list) else -10
+        if rect.collidelistall(door_list):
+            rect.y += 35
+        elif not rect.collidelistall(rect_list):
+            rect.y += speed
+        else:
+            rect.y -= 20
     elif keys[pygame.K_w]:
-        rect.y -= speed if not rect.collidelistall(rect_list) else -10
+        if rect.collidelistall(door_list):
+            rect.y -= 35
+        elif not rect.collidelistall(rect_list):
+            rect.y -= speed
+        else:
+            rect.y += 20
 
     rect.x = max(0, min(rect.x, SCREEN_WIDTH - rect.width))
     rect.y = max(0, min(rect.y, SCREEN_HEIGHT - rect.height))
 
     level.create_rooms()
-    pygame.draw.rect(screen, (255, 0, 0), rect)
+    level.create_doors()
+    player.create_player()
+    screen.blit(telega, rect)
     pygame.display.flip()
     clock.tick(60)
 
