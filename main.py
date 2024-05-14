dfce85ac8e27816481af106a78a64e6bad36b255
game552 <57342236+game552@users.noreply.github.com>
2024-05-14 19:51:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ WALL_WIDTH = 300
 WALL_HEIGHT = 200
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 600
-speed = 4
+speed = 3
 
 rooms = [[1, random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)],
          [1, 1, 1, 1],
@@ -27,6 +27,16 @@ class Player:
         return self.player
 
 
+class Enemy:
+    def __init__(self, screen, y1, y2):
+        self.y_top = y1
+        self.y_bot = y2
+        self.screen = screen
+
+    def create_enemy(self):
+        pygame.draw.rect(self.screen, (255, 0, 0), (self.y_top, self.y_bot, 10, 10), 0)
+
+
 class Wall:
     def __init__(self, screen):
         self.screen = screen
@@ -37,8 +47,10 @@ class Create_rooms(Wall):
         super().__init__(screen)
         self.rect_list = []
         self.rooms = rooms
+        self.doors = []
+        self.rooms1 = []
 
-    def create(self):
+    def create_rooms(self):
         y_top = 0
         y_bottom = 200
         x_left = 0
@@ -63,71 +75,113 @@ class Create_rooms(Wall):
                                      (x * WALL_WIDTH + WALL_WIDTH, y * WALL_HEIGHT, 15, WALL_HEIGHT), 0)
                     right_wall = pygame.Rect(x * WALL_WIDTH + WALL_WIDTH, y * WALL_HEIGHT, 15, WALL_HEIGHT)
 
+                    self.rooms1.append(pygame.Rect(x * WALL_WIDTH, y * WALL_HEIGHT, WALL_WIDTH, WALL_HEIGHT))
                     self.rect_list.append(bottom_wall)
                     self.rect_list.append(left_wall)
                     self.rect_list.append(right_wall)
                     self.rect_list.append(top_wall)
 
-                    if x < len(self.rooms[y]) - 1 and self.rooms[y][x + 1]:
-                        pygame.draw.rect(self.screen, (0, 255, 0),
-                                         (x * WALL_WIDTH + WALL_WIDTH - 15, y * WALL_HEIGHT + WALL_HEIGHT // 2 - 5, 45,
-                                          50), 0)
-                    if y < len(self.rooms) - 1 and self.rooms[y + 1][x]:
-                        pygame.draw.rect(self.screen, (0, 255, 0),
-                                         (x * WALL_WIDTH + WALL_WIDTH // 2 - 5, y * WALL_HEIGHT + WALL_HEIGHT - 15, 45,
-                                          50), 0)
-
-            # Update positions for next row of rooms
             y_top += 200
             y_bottom += 200
             x_left += 300
 
+    def create_doors(self):
+        for y, level in enumerate(self.rooms):
+            for x, room in enumerate(level):
+                if room:
+                    if x < len(self.rooms[y]) - 1 and self.rooms[y][x + 1]:
+                        pygame.draw.rect(self.screen, (0, 40, 0),
+                                         (
+                                             x * WALL_WIDTH + WALL_WIDTH - 3.1, y * WALL_HEIGHT + WALL_HEIGHT // 2 - 15,
+                                             22,
+                                             50), 0)
+                        self.doors.append(
+                            pygame.Rect(x * WALL_WIDTH + WALL_WIDTH - 2, y * WALL_HEIGHT + WALL_HEIGHT // 2 - 5, 22,
+                                        50))
+                    if y < len(self.rooms) - 1 and self.rooms[y + 1][x]:
+                        pygame.draw.rect(self.screen, (0, 255, 0),
+                                         (x * WALL_WIDTH + WALL_WIDTH // 2 - 15, y * WALL_HEIGHT + WALL_HEIGHT - 5, 45,
+                                          22), 0)
+                        self.doors.append(
+                            pygame.Rect(x * WALL_WIDTH + WALL_WIDTH // 2 - 15, y * WALL_HEIGHT + WALL_HEIGHT - 5, 45,
+                                        22))
+
     def get_rect_list(self):
         return self.rect_list
+
+    def get_rooms(self):
+        return self.rooms1
+
+    def get_door_list(self):
+        return self.doors
 
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 telega = pygame.image.load('priest1_v1_1.png').convert_alpha()
 clock = pygame.time.Clock()
-player = Player(580, 290, 10, 10, screen)
+player = Player(560, 290, 10, 10, screen)
 rect = player.get_player()
+enemy_list = []
 
 done = False
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
     level = Create_rooms(rooms, screen)
-    level.create()
+    level.create_rooms()
+    level.create_doors()
     rect_list = level.get_rect_list()
+    door_list = level.get_door_list()
+    rooms_list = level.get_rooms()
+
     screen.fill((0, 0, 0))
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
-        if rect.collidelistall(rect_list):
-            rect.x += 10
-        else:
+        if rect.collidelistall(door_list):
+            rect.x -= 37
+        elif not rect.collidelistall(rect_list):
             rect.x -= speed
+        else:
+            rect.x += 20
     elif keys[pygame.K_d]:
-        if rect.collidelistall(rect_list):
-            rect.x -= 10
-        else:
+        if rect.collidelistall(door_list):
+            rect.x += 37
+        elif not rect.collidelistall(rect_list):
             rect.x += speed
+        else:
+            rect.x -= 20
     elif keys[pygame.K_s]:
-        if rect.collidelistall(rect_list):
-            rect.y -= 10
-        else:
+        if rect.collidelistall(door_list):
+            rect.y += 37
+        elif not rect.collidelistall(rect_list):
             rect.y += speed
-    elif keys[pygame.K_w]:
-        if rect.collidelistall(rect_list):
-            rect.y += 10
         else:
+            rect.y -= 20
+    elif keys[pygame.K_w]:
+        if rect.collidelistall(door_list):
+            rect.y -= 37
+        elif not rect.collidelistall(rect_list):
             rect.y -= speed
+        else:
+            rect.y += 20
 
     rect.x = max(0, min(rect.x, SCREEN_WIDTH - rect.width))
     rect.y = max(0, min(rect.y, SCREEN_HEIGHT - rect.height))
 
-    level.create()
+    for i in range(len(rooms_list)):
+        if len(rooms_list) > len(enemy_list):
+            center = rooms_list[i].center
+            Enemy(screen, center[0] + random.randint(0, 60), center[1] + random.randint(0, 60)).create_enemy()
+            enemy_list.append([center[0] + random.randint(0, 60), center[1] + random.randint(0, 60)])
+        else:
+            Enemy(screen, enemy_list[i][0], enemy_list[i][1]).create_enemy()
+
+    level.create_rooms()
+    level.create_doors()
     player.create_player()
     screen.blit(telega, rect)
     pygame.display.flip()
